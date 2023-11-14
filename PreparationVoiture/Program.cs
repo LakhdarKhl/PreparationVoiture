553d608ba40182b530306bf9b4d0c848237493fa
Lakhdar Khl <82035622+LakhdarKhl@users.noreply.github.com>
2023-11-14 14:52:34 +0100

    test
    tes 2

---
+++ PreparationVoiture/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Voiture
+{
+    public string Modele { get; set; }
+    public string Motorisation { get; set; }
+    public string Couleur { get; set; }
+
+
+    //ici je fais le constructeur
+    public Voiture (string modele, string motorisation, string couleur)
+    {
+        this.Modele = modele;
+        this.Motorisation = motorisation;
+        this.Couleur = couleur;
+    }
+    
+    //methode d'afficahge de détail de la voiture
+    public virtual void AfficherDetail()
+    {
+        Console.WriteLine($"Voiture {Modele} de couleur {Motorisation} ayant pour motorisation {Motorisation}");
+    }
+}
